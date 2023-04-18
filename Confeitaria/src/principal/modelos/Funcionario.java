ace14db44fbe57e87a3176607a53e6f9448f3890
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-18 09:15:40 -0300

    Update Funcionario.java

--- Confeitaria/src/principal/modelos/Funcionario.java
+++ Confeitaria/src/principal/modelos/Funcionario.java
@@ -1,5 +1,141 @@
 package principal.modelos;
-
 public class Funcionario {
-
+private Pessoa pessoa;
+private int idFuncionario;
+private String funcao;
+private String periodo;
+private double salario;
+public Funcionario() {
+}
+public Funcionario(Pessoa pessoa, int idFuncionario, String funcao, String periodo, double salario) {
+this.pessoa = pessoa;
+this.idFuncionario = idFuncionario;
+this.funcao = funcao;
+this.periodo = periodo;
+this.salario = salario;
+}
+public Pessoa getPessoa() {
+return pessoa;
+}
+public void setPessoa(Pessoa pessoa) {
+this.pessoa = pessoa;
+}
+public int getIdFuncionario() {
+return idFuncionario;
+}
+public void setIdFuncionario(int idFuncionario) {
+this.idFuncionario = idFuncionario;
+}
+public String getFuncao() {
+return funcao;
+}
+public void setFuncao(String funcao) {
+this.funcao = funcao;
+}
+public String getPeriodo() {
+return periodo;
+}
+public void setPeriodo(String periodo) {
+this.periodo = periodo;
+}
+public double getSalario() {
+return salario;
+}
+public void setSalario(double salario) {
+this.salario = salario;
+}
+}
+package principal.modelos;
+public class Funcionario {
+private Pessoa pessoa;
+private int idFuncionario;
+private String funcao;
+private String periodo;
+private double salario;
+public Funcionario() {
+}
+public Funcionario(Pessoa pessoa, int idFuncionario, String funcao, String periodo, double salario) {
+this.pessoa = pessoa;
+this.idFuncionario = idFuncionario;
+this.funcao = funcao;
+this.periodo = periodo;
+this.salario = salario;
+}
+public Pessoa getPessoa() {
+return pessoa;
+}
+public void setPessoa(Pessoa pessoa) {
+this.pessoa = pessoa;
+}
+public int getIdFuncionario() {
+return idFuncionario;
+}
+public void setIdFuncionario(int idFuncionario) {
+this.idFuncionario = idFuncionario;
+}
+public String getFuncao() {
+return funcao;
+}
+public void setFuncao(String funcao) {
+this.funcao = funcao;
+}
+public String getPeriodo() {
+return periodo;
+}
+public void setPeriodo(String periodo) {
+this.periodo = periodo;
+}
+public double getSalario() {
+return salario;
+}
+public void setSalario(double salario) {
+this.salario = salario;
+}
+}
+package principal.modelos;
+public class Funcionario {
+private Pessoa pessoa;
+private int idFuncionario;
+private String funcao;
+private String periodo;
+private double salario;
+public Funcionario() {
+}
+public Funcionario(Pessoa pessoa, int idFuncionario, String funcao, String periodo, double salario) {
+this.pessoa = pessoa;
+this.idFuncionario = idFuncionario;
+this.funcao = funcao;
+this.periodo = periodo;
+this.salario = salario;
+}
+public Pessoa getPessoa() {
+return pessoa;
+}
+public void setPessoa(Pessoa pessoa) {
+this.pessoa = pessoa;
+}
+public int getIdFuncionario() {
+return idFuncionario;
+}
+public void setIdFuncionario(int idFuncionario) {
+this.idFuncionario = idFuncionario;
+}
+public String getFuncao() {
+return funcao;
+}
+public void setFuncao(String funcao) {
+this.funcao = funcao;
+}
+public String getPeriodo() {
+return periodo;
+}
+public void setPeriodo(String periodo) {
+this.periodo = periodo;
+}
+public double getSalario() {
+return salario;
+}
+public void setSalario(double salario) {
+this.salario = salario;
+}
 }
