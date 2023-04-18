bab45e5310c784ff1f9bf5fbc0982a164ce87065
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-18 09:13:35 -0300

    Update Cliente.java

--- Confeitaria/src/principal/modelos/Cliente.java
+++ Confeitaria/src/principal/modelos/Cliente.java
@@ -1,5 +1,31 @@
 package principal.modelos;
 
 public class Cliente {
+    private Pessoa pessoa;
+    private int idCliente;
+ 
+  public Cliente() {
+  }
 
+  public Cliente(Pessoa pessoa, int idCliente) {
+    this.pessoa = pessoa;
+    this.idCliente = idCliente;
+  }
+
+  public Pessoa getPessoa() {
+    return pessoa;
+   }
+
+  public void setPessoa(Pessoa pessoa) {
+    this.pessoa = pessoa;
+  }
+
+  public int getIdCliente() {
+    return idCliente;
+  }
+ 
+  public void setIdCliente(int idCliente) {
+    this.idCliente = idCliente;
+  }
 }
+
