ae1dddceeab5ebd078609be4256eff1fca59b1e0
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-25 10:41:45 -0300

    Update Pessoa.java

--- Confeitaria/src/principal/modelos/Pessoa.java
+++ Confeitaria/src/principal/modelos/Pessoa.java
@@ -1,24 +1,38 @@
 package principal.modelos;
+	
 public class Pessoa {
-private String nome;
-private String telefone;
-public Pessoa(){
+	
+	private String nome;
+	private String telefone;
+	
+	
+	public Pessoa(){
+	}
+	
+	
+	public Pessoa(String nome, String telefone) {
+		this.nome = nome;
+		this.telefone = telefone;
+	}
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getTelefone() {
+		return telefone;
+	}
+	
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
 }
-public Pessoa(String nome, String telefone) {
-this.nome = nome;
-this.telefone = telefone;
-}
-public String getNome() {
-return nome;
-}
-public void setNome(String nome) {
-this.nome = nome;
-}
-public String getTelefone() {
-return telefone;
-}
-public void setTelefone(String telefone) {
-this.telefone = telefone;
-}
-}
+
+
 
