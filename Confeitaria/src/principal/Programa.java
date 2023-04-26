4911dfe21d8204269e3b3f3bc90a68e4bd8b080e
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-25 22:09:58 -0300

    Update Programa.java

--- Confeitaria/src/principal/Programa.java
+++ Confeitaria/src/principal/Programa.java
@@ -1,13 +1,40 @@
 package principal;
-
 import principal.modelos.Pessoa;
+import java.util.Scanner;
+
+import Funcao.FuncaoProd;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import principal.modelos.Funcionario;
 import principal.modelos.Cliente;
+import principal.modelos.Produto;
+import principal.modelos.Item_Pedido;
+import principal.modelos.Estoque;
+import principal.modelos.Pedido;
+import Funcao.FuncaoCliente;
+import Funcao.FuncaoFuncionario;
 
-  public class Programa {
-    public static void main(String[] args) {
-      
-      
-    }
-  }
+public class Programa {
+	public static void main(String[] args) throws FileNotFoundException {
+		
+		FuncaoProd Cardapio = new FuncaoProd();
+		
+		FuncaoCliente HistoricoClientes = new FuncaoCliente();
+		
+		FuncaoFuncionario ArquivoFuncionarios = new FuncaoFuncionario();
+		
+		//Mostrar o cardapio
+		//Cardapio.LerProdutos();
+		
+		//Mostrar Historico de Clientes
+		//HistoricoClientes.LerClientes();
+		
+		//Mostrar Arquivo de Funcionarios
+		ArquivoFuncionarios.LerFuncionario();
+		
+		
+	}
+
+}
 
