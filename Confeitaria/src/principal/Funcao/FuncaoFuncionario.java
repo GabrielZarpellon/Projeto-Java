896d46fbdbdc98bfde6ced424461daea5aea121e
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-25 22:09:15 -0300

    Add files via upload

---
+++ Confeitaria/src/principal/Funcao/FuncaoFuncionario.java
@@ -0,0 +1,57 @@
+package Funcao;
+
+import java.io.File;
+import java.util.Scanner;
+import principal.modelos.Funcionario;
+
+import java.io.FileNotFoundException;
+
+public class FuncaoFuncionario {
+	public static void LerFuncionario() throws FileNotFoundException{
+		
+		String arquivoFuncionarios = "C:\\Users\\Acer\\git\\Teste\\Confeitaria\\src\\arquivosTxt\\Funcionarios";
+		File arquivo = new File(arquivoFuncionarios);
+		Scanner funcionarios = new Scanner(arquivo);
+		
+		Funcionario[] vetorDeFuncionarios = new Funcionario[5];
+		
+		int cont = 0;
+		
+		while(funcionarios.hasNextLine()) {
+			
+			try {
+			String linha = funcionarios.nextLine();
+			String[] valores = linha.split("\t");
+			
+			Funcionario f = new Funcionario();
+			f.setIdFuncionario(Integer.parseInt(valores[0]));
+			f.setNome(valores[1]);
+			f.setTelefone(valores[2]);
+			f.setFuncao(valores[3]);
+			f.setPeriodo(valores[4]);
+			f.setSalario(Double.parseDouble(valores[5]));
+			
+			vetorDeFuncionarios[cont] = f;
+			cont++;
+			}catch(NumberFormatException e) {
+				System.out.println("Erro ao ler o produto na linha " + cont + ": " + e.getMessage());
+			}
+		}
+		
+		
+		System.out.println("Arquivo de Funcionarios: \n\n");
+		
+		for(int i = 0; i < vetorDeFuncionarios.length; i++) {
+			
+			Funcionario f = vetorDeFuncionarios[i];
+			System.out.println(f.getIdFuncionario() + "\t" + f.getNome() + "\t" + f.getTelefone() + "\t" + f.getFuncao() + "\t" + f.getPeriodo() + "\t" + f.getSalario());
+			System.out.println();
+			
+		}
+		
+		funcionarios.close();
+		
+		
+	}
+
+}
---
+++ Confeitaria/src/principal/Funcao/FuncaoProd.java
@@ -0,0 +1,58 @@
+package Funcao;
+
+import java.io.File;
+
+import java.util.Scanner;
+
+import principal.modelos.Produto;
+
+import java.io.FileNotFoundException;
+
+public class FuncaoProd {
+public static void LerProdutos () throws FileNotFoundException {
+	String arquivoProdutos = "C:\\Users\\Acer\\git\\Teste\\Confeitaria\\src\\arquivosTxt\\Produtos";
+	File arquivo = new File(arquivoProdutos);
+	Scanner produtos = new Scanner(arquivo);
+	
+	Produto[] vetorDeProdutos = new Produto[23];
+	
+	int cont = 0;
+	
+	while(produtos.hasNextLine()) {
+		
+		try {
+		String linha = produtos.nextLine();
+		String[] valores = linha.split("\t");
+		
+		Produto p = new Produto();
+		p.setId_produto(Integer.parseInt(valores[0]));
+		p.setNome(valores[1]);
+		p.setPreco(Double.parseDouble(valores[2]));
+		p.setTipo(valores[3]);
+		
+		vetorDeProdutos[cont] = p;
+		cont++;
+		}catch(NumberFormatException e) {
+			System.out.println("Erro ao ler o produto na linha " + cont + ": " + e.getMessage());
+		}
+	}
+	
+	
+	System.out.println("Lista de Produtos: \n\n");
+	
+	for(int i = 0; i < vetorDeProdutos.length; i++) {
+		
+		Produto p = vetorDeProdutos[i];
+		System.out.println(p.getId_produto() + "\t" + p.getNome() + "\t" + p.getPreco() + "\t" + p.getTipo());
+		System.out.println();
+		
+	}
+	
+	
+	
+	produtos.close();
+	
+	
+}
+
+}
