9648c6cbca4788b171d2e6ee4eabbbd4332ac5a4
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-27 11:40:50 -0300

    Update Estoque.java

--- Confeitaria/src/principal/modelos/Estoque.java
+++ Confeitaria/src/principal/modelos/Estoque.java
@@ -2,17 +2,40 @@ package principal.modelos;
 
 import java.util.ArrayList;
 import java.util.List;
+import principal.modelos.Produto;
+import funcao.FuncaoEstoque;
 
-public class Estoque {
-	private List<Produto> Produtos = new ArrayList<>();
+public class Estoque{
 
-	public List<Produto> getProdutos() {
-		return Produtos;
-	}
-
-	public void setProdutos(List<Produto> produtos) {
-		Produtos = produtos;
+	private String nome;
+	private int quantidade;
+	
+	public Estoque(){
+		
 	}
 	
+	public Estoque(String nome, int quantidade ) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
 	
 }
