136e1ca59932bf2ebe17f1fb7daafebdb40be921
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-04-27 11:44:55 -0300

    Add files via upload

--- Confeitaria/src/principal/Programa.java
+++ Confeitaria/src/principal/Programa.java
@@ -3,7 +3,10 @@ import java.util.Scanner;
 import principal.modelos.Pessoa;
 import java.util.Scanner;
 
-import Funcao.FuncaoProd;
+import java.util.ArrayList;
+import java.util.List;
+
+import funcao.FuncaoProd;
 
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -13,13 +16,14 @@ import principal.modelos.Produto;
 import principal.modelos.Item_Pedido;
 import principal.modelos.Estoque;
 import principal.modelos.Pedido;
-import Funcao.FuncaoCliente;
-import Funcao.FuncaoFuncionario;
+import funcao.FuncaoCliente;
+import funcao.FuncaoFuncionario;
+import funcao.FuncaoEstoque;
 
 public class Programa {
 	public static void main(String[] args) throws FileNotFoundException {
 		
-		int op;
+		int op; int acesso;
 		
 		Scanner leitor = new Scanner(System.in);
 		
@@ -29,8 +33,17 @@ public class Programa {
 		
 		FuncaoFuncionario ArquivoFuncionarios = new FuncaoFuncionario();
 		
+		Estoque Bolo = new Estoque("Bolo de Cenoura", 12);
+		Estoque agua = new Estoque("agua", 40);
+		
+		FuncaoEstoque lista = new FuncaoEstoque();
+		
+		lista.adicionar(Bolo);
+		lista.adicionar(agua);
+		
+		
 		do {
-			System.out.println("___Menu___\n Selecione uma opção: \n[1]Fazer Pedido\n[2]Ver Cardapio \n[3]Acesso aos Arquivos\n[4]Sair");
+			System.out.println("___Menu___\n Selecione uma opção: \n[1]Fazer Pedido\n[2]Ver Cardapio \n[3]Acesso aos Arquivos\n[4]Listar Estoque \n[5]Sair");
 			op = leitor.nextInt();
 			
 			switch(op) {
@@ -46,11 +59,13 @@ public class Programa {
 				leitor.nextLine();
 				System.out.println("Informe a senha de acesso: ");
 				String senha = leitor.nextLine();
+			
 				
 				if(senha.equals("acessoPermitido")) {
+					do {
 					System.out.println("Informe o que gostaria de acessar: \n[1]Historico de Clientes \n[2]Historico de pedidos \n[3]Arquivo de Funcionarios \n[4]Sair");
-					int acesso = leitor.nextInt();
-					
+					acesso = leitor.nextInt();
+										
 					switch(acesso) {
 					case 1:
 							HistoricoClientes.LerClientes();
@@ -61,23 +76,39 @@ public class Programa {
 					case 3:
 							ArquivoFuncionarios.LerFuncionario();
 						break;
+					case 4:
+						
+						break;
+					
 					}//switch
 
+					}while(acesso != 4);
 				}else {
 					System.out.println("Acesso Negado, Senha incorreta!");
 				}
 				
+				
 				break;
 				
 			case 4:
+					List<Estoque> lista2 = lista.getProdutos();
+					for(int i = 0; i < lista2.size(); i++) {
+						Estoque estoque = lista2.get(i);
+						System.out.println("Ingrediente: " + estoque.getNome() + "\nQuantidade: " + estoque.getQuantidade());
+					}
+				
 				break;
+			
+			case 5:
+					
+				break;
+				
 			default:
 				System.out.println("Opção Invalida!");
 			break;
 			}
 			
 		}while(op != 4);
-		
 		
 		}
 	}
