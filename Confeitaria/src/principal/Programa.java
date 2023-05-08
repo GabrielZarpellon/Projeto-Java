7a945d529e87bc1a3d77d5da246f70717229bd7b
MaryBra <117322128+MaryBra@users.noreply.github.com>
2023-05-08 20:07:04 -0300

    Update Programa.java

--- Confeitaria/src/principal/Programa.java
+++ Confeitaria/src/principal/Programa.java
@@ -1,13 +1,11 @@
 package principal;
+
 import java.util.Scanner;
 import principal.modelos.Pessoa;
 import java.util.Scanner;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import funcao.FuncaoProd;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import principal.modelos.Funcionario;
@@ -19,6 +17,8 @@ import principal.modelos.Pedido;
 import funcao.FuncaoCliente;
 import funcao.FuncaoFuncionario;
 import funcao.FuncaoEstoque;
+import funcao.FuncaoListaEstoque;
+import funcao.FuncaoPedido;
 
 public class Programa {
 	public static void main(String[] args) throws FileNotFoundException {
@@ -27,98 +27,75 @@ public class Programa {
 		
 		Scanner leitor = new Scanner(System.in);
 		
+		//Criando as funcoes
+		
 		FuncaoProd Cardapio = new FuncaoProd();
-		
 		FuncaoCliente HistoricoClientes = new FuncaoCliente();
-		
 		FuncaoFuncionario ArquivoFuncionarios = new FuncaoFuncionario();
+		FuncaoListaEstoque ControleEstoque = new FuncaoListaEstoque();
+		FuncaoPedido RealizarPedido = new FuncaoPedido();
 		
-		Estoque Bolo = new Estoque("Bolo de Cenoura", 12);
-		Estoque agua = new Estoque("agua", 40);
-		
-		FuncaoEstoque lista = new FuncaoEstoque();
-		
-		lista.adicionar(Bolo);
-		lista.adicionar(agua);
-		
+		//Loop Menu Principal
 		
 		do {
-			System.out.println("___Menu___\n Selecione uma opção: \n[1]Fazer Pedido\n[2]Ver Cardapio \n[3]Acesso aos Arquivos\n[4]Listar Estoque \n[5]Sair");
+			System.out.println("*** Menu ***\nSelecione uma opção: \n[1]Fazer Pedido\n[2]Ver Cardapio \n[3]Acesso aos Arquivos\n[4]Controle do Estoque \n[5]Sair");
 			op = leitor.nextInt();
 			
 			switch(op) {
-			case 1:
+				case 1:
+						RealizarPedido.FazerPedido();
+					break;
+				
+				case 2:
+						Cardapio.LerProdutos();
+					break;
 					
-				break;
-				
-			case 2:
-					Cardapio.LerProdutos();
-				break;
-				
-			case 3:
-				leitor.nextLine();
-				System.out.println("Informe a senha de acesso: ");
-				String senha = leitor.nextLine();
+				case 3:
+					leitor.nextLine();
+					System.out.println("Informe a senha de acesso: ");
+					String senha = leitor.nextLine();
 			
-				
-				if(senha.equals("acessoPermitido")) {
-					do {
-					System.out.println("Informe o que gostaria de acessar: \n[1]Historico de Clientes \n[2]Historico de pedidos \n[3]Arquivo de Funcionarios \n[4]Sair");
-					acesso = leitor.nextInt();
+						if(senha.equals("Permitido")) {
+							do {
+								System.out.println("Informe o que gostaria de acessar: \n[1]Historico de Clientes \n[2]Historico de pedidos \n[3]Arquivo de Funcionarios \n[4]Sair");
+								acesso = leitor.nextInt();
 										
-					switch(acesso) {
-					case 1:
-							HistoricoClientes.LerClientes();
-						break;
-					case 2:
+									switch(acesso) {
+										case 1:
+												HistoricoClientes.LerClientes();
+											break;
+										case 2:
 						
-						break;
-					case 3:
-							ArquivoFuncionarios.LerFuncionario();
-						break;
-					case 4:
-						
-						break;
-					
-					}//switch
+											break;
+										case 3:
+												ArquivoFuncionarios.LerFuncionario();
+											break;
+										case 4:
+											break;
+									}//switch
 
-					}while(acesso != 4);
-				}else {
-					System.out.println("Acesso Negado, Senha incorreta!");
-				}
-				
-				
-				break;
-				
-			case 4:
-					List<Estoque> lista2 = lista.getProdutos();
-					for(int i = 0; i < lista2.size(); i++) {
-						Estoque estoque = lista2.get(i);
-						System.out.println("Ingrediente: " + estoque.getNome() + "\nQuantidade: " + estoque.getQuantidade());
-					}
-				
-				break;
-			
-			case 5:
-					
-				break;
-				
-			default:
-				System.out.println("Opção Invalida!");
-			break;
-			}
-			
-		}while(op != 4);
-		
-		}
-	}
+								}while(acesso != 4);//do
+							
+						}else {
+							System.out.println("Acesso Negado, Senha incorreta!");
+						}
 	
-	//Mostrar o cardapio
-			//Cardapio.LerProdutos();
+					break;
+				
+				case 4:
+						ControleEstoque.MenuEstoque();
+					break;
 			
-			//Mostrar Historico de Clientes
-			//HistoricoClientes.LerClientes();
+				case 5:
+					break;
+				
+				default:
+					System.out.println("Opção Invalida!");
+				break;
+			}//switch
 			
-			//Mostrar Arquivo de Funcionarios
-			//ArquivoFuncionarios.LerFuncionario();
+		}while(op != 5);
+		
+	}
+}
 
